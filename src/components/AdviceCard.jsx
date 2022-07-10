f9486fe8b7ecd4dd3cfc789a6038be859329af9b
Juan P. Isaza <jisazamp@gmail.com>
2022-07-10 17:41:26 -0500

    Set up the call to the API

--- src/components/AdviceCard.jsx
+++ src/components/AdviceCard.jsx
@@ -1,21 +1,41 @@
+import { useState, useEffect } from 'react';
+
 import Separator from '../images/pattern-divider-mobile.svg';
 import IconDice from '../images/icon-dice.svg';
 
 const AdviceCard = () => {
-  return (
-    <article className='flex flex-col relative items-center card w-[343px] h-[315px] rounded-md'>
-      <p className='mt-10 text-[11px] uppercase'>Advice #</p>
+  const [advice, setAdvice] = useState({});
 
-      <h1 className='mt-6 text-2xl text-center pl-4 pr-4'>
-        “It is easy to sit up and take notice, what's difficult is getting up
-        and taking action.”
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = async () => {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+    setAdvice(data.slip);
+  };
+
+  return (
+    <article className='flex flex-col relative items-center card w-[343px] rounded-md'>
+      <p className='mt-10 text-[11px] uppercase'>Advice #{advice.id}</p>
+
+      <h1 className='mt-6 text-2xl text-center pl-4 pr-4 mb-32'>
+        "{advice.advice}"
       </h1>
 
-      <img className='mt-6' src={Separator} alt='Separator' />
+      <img
+        className='mt-6 absolute bottom-16'
+        src={Separator}
+        alt='Separator'
+      />
 
-      <div className='flex justify-center absolute -bottom-7 items-center h-16 w-16 rounded-full'>
+      <button
+        onClick={() => fetchAdvice()}
+        className='flex justify-center absolute -bottom-7 items-center h-16 w-16 rounded-full'
+      >
         <img src={IconDice} alt='Dice' />
-      </div>
+      </button>
     </article>
   );
 };
